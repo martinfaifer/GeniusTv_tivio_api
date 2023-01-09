9f7058a7ecd1f030910d37e09b9c8cd855710fd2
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-01-09 11:59:48 +0100

    added ispId for show nangu user, for specific isp

--- app/Actions/Subscriptions/FindSubscriptionByIdentityAction.php
+++ app/Actions/Subscriptions/FindSubscriptionByIdentityAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Subscriptions;
 
+use App\Models\Subscriber;
 use App\Models\SubscriptionIdentity;
 
 class FindSubscriptionByIdentityAction
@@ -9,7 +10,8 @@ class FindSubscriptionByIdentityAction
     public function execute(int $identityId)
     {
         $identity = SubscriptionIdentity::find($identityId);
+        $subscriber = Subscriber::find($identity->subscription->subscriberId);
 
-        return "fake_~~" . $identity->subscription->subscriptionCode;
+        return $subscriber->ispId . "~~" . $identity->subscription->subscriptionCode;
     }
 }
--- app/Services/NanguWsdl/Subscriptions/NanguWsdlShowSubscriptionInfoService.php
+++ app/Services/NanguWsdl/Subscriptions/NanguWsdlShowSubscriptionInfoService.php
@@ -9,7 +9,7 @@ class NanguWsdlShowSubscriptionInfoService extends NanguWsdlService
     public function show(string $subscriptionCode)
     {
         $subscriptionCodeArray = explode("~~", $subscriptionCode);
-        $params = array('subscriptionCode' => array('subscriptionCode' => $subscriptionCodeArray[1]));
+        $params = array('subscriptionCode' => array('subscriptionCode' => $subscriptionCodeArray[1], 'ispCode' => $subscriptionCodeArray[0]));
         $data = $this->soap->__soapCall('getInfo', $params);
         $wsdlResult = json_decode(json_encode($data), true);
         return $wsdlResult;
